1d598a8fa462365c39a2f9529af6d8f0539d503a
Jakub <kubplon@gmail.com>
2020-03-08 14:36:20 +0100

    Initial commit

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,21 @@
+package pl.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         1. Utworzyc klase Student zawierajaca pola:
+           - name
+           - surname
+           - grades (lista Integerow)
+         2. w mainie utworz 10 studentow i dodaj ich do listy students
+         3. w klasie main utworz funkcje pozwalajaca wyliczac srednia oceny studentow
+         4. w klasie main utworz funkcje wyszukujaca studentow wedlug podanego imienia
+         5. W konsoli stworz "interfejs" pozwalajacy na dodawanie i usuwanie studentow
+         6. zamiast tworzyc 10 studentow "recznie" 10 razy wywolujac "new" odczytaj ich z pliku o strukturze:
+            imie;nazwisko;ocena1,ocena2,ocena3
+            np.
+            Jakub;Plonka;1,2,3,4,5,6,2,2,4
+            Jakub;Test;1,4,3,4,5,3,2,2,4
+         */
+    }
+}
